0614481f22e163ad1f3b818e310beefbcab6b80c
SAGUN JAISWAL <sagun.jaiswal@aot.edu.in>
2022-04-05 14:40:04 +0530

    Update Program1.js

--- Program1.js
+++ Program1.js
@@ -10,6 +10,8 @@ Output: String with comma delimited elements of the array in th same order.
 "h,o,l,a"
 */
 
+//SOLUTION 1 :
+
 function printArray(array){
   let resStr = "";
   for(let i  = 0 ; i < array.length ; i++){
@@ -20,3 +22,33 @@ function printArray(array){
   }
   return resStr;
 }
+
+//SOLUTION 2:
+function printArray(array){
+  return array.join();
+}
+/*
+join() :
+The arr.join() method is used to join the elements of an array into a string. The elements of the string will be separated by a specified separator and its 
+default value is a comma(, ).
+array.join(separator)
+
+INPUT :
+var a = [1, 2, 3, 4, 5, 6];
+print(a.join('|'));
+OUTPUT:
+1|2|3|4|5|6
+
+
+INPUT :
+var a = [1, 2, 3, 4, 5, 6];
+print(a.join());
+OUTPUT:
+1, 2, 3, 4, 5, 6
+
+INPUT :
+var a = [1, 2, 3, 4, 5, 6];
+print(a.join(''));
+OUTPUT:
+123456
+*/
