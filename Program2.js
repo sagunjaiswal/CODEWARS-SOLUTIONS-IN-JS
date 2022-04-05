46fee1ed748999ef15fe2e811c011d45a8dc54d8
SAGUN JAISWAL <sagun.jaiswal@aot.edu.in>
2022-04-05 14:43:52 +0530

    Create Program2.js

---
+++ Program2.js
@@ -0,0 +1,14 @@
+/*
+LINK : https://www.codewars.com/kata/opposite-number
+QUESTION :
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+function opposite(number) {
+  return(-number);
+}
