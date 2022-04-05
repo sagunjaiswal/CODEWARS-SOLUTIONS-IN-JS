b160cad5cc56a155a8b51c4de8fc67bb0083a978
SAGUN JAISWAL <sagun.jaiswal@aot.edu.in>
2022-04-05 14:50:16 +0530

    Create Program3.js

---
+++ Program3.js
@@ -0,0 +1,33 @@
+/*
+LINK : https://www.codewars.com/kata/basic-mathematical-operations
+QUESTION :
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+*/
+function basicOp(operation, value1, value2)
+{
+  let res = 0;
+  switch(operation){
+      case '+':
+        res = value1+value2;
+        break;
+      case '-':
+        res = value1-value2;
+        break;
+      case '*':
+        res = value1*value2;
+        break;
+      case '/':
+        res = value1/value2;
+        break;
+  }
+  return res;
+}
